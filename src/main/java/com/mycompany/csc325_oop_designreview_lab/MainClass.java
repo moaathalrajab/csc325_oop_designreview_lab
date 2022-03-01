a5cc29fa783360674367c70926b9dde87249ef19
Moaath Alrajab <moaath@alrajab.net>
2022-03-01 10:55:10 -0500

    Update Todo list

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -14,19 +14,29 @@ public class MainClass {
 		// ToDo 5: Fix the error
 		
 		// ToDo 6: Fix the constructor of Student class
+                
+                // Todo 7: Create two classes for Freshman and Senior 
+                
+                // ToDo 8: The senior class should have a minimum of 85 credits  
 		
-		// ToDo 7: Add a toString method for Student class
+		// ToDo 9: Add a toString method for Freshman class
 		
-		Student std1= new Student("James", 20);
+                // ToDo 10: Add a toString method for Senior class
+                
+		Freshman std1= new Student("James", 20, 12); // name, age, credits
+                
+                Senior std2 = new Student("John", 30, 90);
 		
-		// ToDo 8: Set the gpa of the student using the scanner and user
+		// ToDo 11: Set the gpa of the student using the scanner and user
 		// 			input and then print the output.
 		
 		System.out.println(std1);
+                
+                System.out.println(std2);
 		
-		// ToDo 9: add comments and explain your code
+		// ToDo 12: add comments and explain your code
 		
-		// ToDo 10: submit using a pull request.
+		// ToDo 13: submit using a pull request.
 	}
 
 }
--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -10,7 +10,7 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public class Student {
-	// ToDo 1: Make this class a child of Person
+	// ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
 	
