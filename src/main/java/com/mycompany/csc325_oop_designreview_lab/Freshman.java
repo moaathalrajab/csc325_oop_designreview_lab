2f57f340b477e7a472c3b12296cfb98636656304
mikelanasa <mikelanasa@gmail.com>
2023-03-10 16:48:03 -0500

    created Freshman class as a child of Student includes basic constructor

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_oop_designreview_lab;
+
+/**
+ *
+ * @author Michael LaNasa
+ */
+public class Freshman extends Student{
+    // Constructor that takes the minimum of 2 parameters for name and age
+    public Freshman(String name, int age) {
+        super(name, age);
+    }
+    
+}
