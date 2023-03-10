cf7a1849d14af40d614cbe5476d15a534e11a61c
mikelanasa <mikelanasa@gmail.com>
2023-03-10 16:48:50 -0500

    created Senior class as a child of Student includes basic constructor

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_oop_designreview_lab;
+
+/**
+ *
+ * @author Michael LaNasa
+ */
+public class Senior extends Student{
+    // Constructor that takes the minimum of 2 parameters for name and age
+    public Senior(String name, int age) {
+        super(name, age);
+    }
+}
