bfd03b33b200ecaeaa40f6c8b9a0fd4188728098
mikelanasa <mikelanasa@gmail.com>
2023-03-10 17:03:45 -0500

    added toString method

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -9,9 +9,27 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author Michael LaNasa
  */
 public class Freshman extends Student{
-    // Constructor that takes the minimum of 2 parameters for name and age
+    /*
+    constructor takes in the minimum 2 parameters for name and age
+    */
     public Freshman(String name, int age) {
         super(name, age);
     }
     
+    /*
+    constructor takes in 3 parameters for name, age, and credits
+    if credits are below 85 system will print an error message
+    */
+    public Freshman(String name, int age, int credits) {
+        super(name, age, credits);
+    }
+    
+    /* 
+    toString method
+    Returns Freshman member variables in tidy String format
+    */
+    @Override
+    public String toString() {
+        return super.toString() + "Freshman Credits: " + this.getCredits() + "\n";
+    }
 }
