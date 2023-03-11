7911a11482ae62fdf0577f4263633a3bc6d090ed
Jake Rios <jakerios01@gmail.com>
2023-03-10 22:20:08 -0500

    Completed to-do tasks 6 - 13

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.csc325_oop_designreview_lab;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MoaathAlrajab
@@ -22,20 +24,33 @@ public class MainClass {
 		// ToDo 7: Add a toString method for Student class
 		// ToDo 9: Add a toString method for Freshman class
 
-		Student std1= new Student("James", 20);
+		Student std1= new Student("James", (short)20);
                 // ToDo 10: Add a toString method for Senior class
 
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
+		Student std2= new Freshman("James", (short)20, 12); // name, age, credits
 
-                Senior std2 = new Student("John", 30, 90);
+                Student std3 = new Senior("John", (short)30, 90);
 
 		// ToDo 8: Set the gpa of the student using the scanner and user
 		// ToDo 11: Set the gpa of the student using the scanner and user
 		// 			input and then print the output.
+                
+                //Creating a scanner
+                Scanner scnr = new Scanner(System.in);
+                
+                //Asking to enter GPA of first student
+                System.out.println("Please enter the student GPA: ");
+                double x = scnr.nextInt();
+                std3.setGPA(x);
+                
+                //Asking to enter GPA of the second student
+                System.out.println("Please enter the student GPA: ");
+                double y = scnr.nextInt();
+                std3.setGPA(y);
+                
+		System.out.println(std2);
 
-		System.out.println(std1);
-
-                System.out.println(std2);
+                System.out.println(std3);
 
 		// ToDo 9: add comments and explain your code
 		// ToDo 12: add comments and explain your code
