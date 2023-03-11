cc41869265516f884a29d9be7611f1ec4d2867d9
TLuu52 <luuthuan817@gmail.com>
2023-03-10 23:31:59 -0500

    Fixed some errors in the comments

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -9,11 +9,12 @@ import java.util.Scanner;
 public class MainClass {
 
     public static void main(String[] args) {
+        
         //Constructor calls
         Freshman std1 = new Freshman("Sarah", 15, "42 Michagan Dr", 12);
         Senior std2 = new Senior("John", 18, "34 Pine Ave", 90);
 
-        //Create scanenr for input
+        //Creates a scanner for input
         Scanner input = new Scanner(System.in);
 
         //Take inputs
@@ -23,11 +24,11 @@ public class MainClass {
         System.out.println("Enter a number to set the GPA of student #2: ");
         double grade2 = input.nextDouble();
 
-        //Set Student GPAs
+        //Sets the Student GPAs
         std1.setGPA(grade1);
         std2.setGPA(grade2);
 
-        //Print results with calls to toString()
+        //Prints results with calls to toString()
         System.out.println(std1);
         System.out.println(std2);
     }
