17d8fba33ed69e7b4832080a105f0d5a0988d9e6
Jake Rios <jakerios01@gmail.com>
2023-03-10 21:51:43 -0500

    Completed to-do tasks 1 - 5

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -8,13 +8,60 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
+public class Student extends Human {
 	// ToDo 1: Make this class a child of Person
 	// ToDo 1: Make this class a child of Human
 
 	// ToDo 2: Fix the resulting errors
+    
+        //Fields specific for the Student class
+        private String address;
+        private double gpa;
+        
+        /**
+         * Basic constructor for Student object
+         * @param name
+         * @param age 
+         */
+        public Student(String name, short age) {
+            super(name, age);
+        }
+        
+        /**
+         * Returns address field
+         * @return 
+         */
+        @Override
+        public String getAddress() {
+            return this.address;
+        }
+        
+        /**
+         * Setter for address field
+         * @param address 
+         */
+        @Override
+        public void setAddress(String address) {
+            this.address = address;
+        }
 
 	// ToDo 3: Add a field for GPA and create setter and getter
+        
+        /**
+         * Setter for GPA field
+         * @param gpa 
+         */
+        public void setGPA(double gpa) {
+            this.gpa = gpa;
+        }
+        
+        /**
+         * Getter for GPA field
+         * @return 
+         */
+        public double getGPA() {
+            return this.gpa;
+        }
 	
 	// ToDo 4: Add comments to your code
 }
