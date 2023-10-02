8272494f43376b88b47bf6629d9ea07ce8d4ad48
Moaath Alrajab <41642853+moaathalrajab@users.noreply.github.com>
2023-10-01 22:47:55 -0400

    Update MainClass.java
    Update numbering of ToDos

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -13,35 +13,32 @@ public class MainClass {
  public static void main(String[] args) {
 		// ToDo 5: Fix the error
 
-		// ToDo 6: Fix the constructor of Student class
+		// ToDo 6: Fix the constructor of the Student class
 
                 // Todo 7: Create two classes for Freshman and Senior 
 
                 // ToDo 8: The senior class should have a minimum of 85 credits  
 
-		// ToDo 7: Add a toString method for Student class
-		// ToDo 9: Add a toString method for Freshman class
+		// ToDo 9: Add a toString method for the Student class
+		// ToDo 10: Add a toString method for the Freshman class
 
 		Student std1= new Student("James", 20);
-                // ToDo 10: Add a toString method for Senior class
+                // ToDo 11: Add a toString method for the Senior class
 
 		Freshman std1= new Student("James", 20, 12); // name, age, credits
 
                 Senior std2 = new Student("John", 30, 90);
 
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// ToDo 11: Set the gpa of the student using the scanner and user
+		// ToDo 12: Set the gpa of the student using the scanner and user
 		// 			input and then print the output.
 
 		System.out.println(std1);
 
                 System.out.println(std2);
 
-		// ToDo 9: add comments and explain your code
-		// ToDo 12: add comments and explain your code
+		// ToDo 13: add comments and explain your code
 
-		// ToDo 10: submit using a pull request.
-		// ToDo 13: submit using a pull request.
+		// ToDo 14: submit using a pull request.
 	}
 
 }
