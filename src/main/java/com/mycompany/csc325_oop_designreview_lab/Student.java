87ce5454079028d2162acc91cfd0fc8a5ad93555
Moaath Alrajab <41642853+moaathalrajab@users.noreply.github.com>
2023-10-01 22:49:04 -0400

    Update Student.java
    Fix ToDos

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -9,12 +9,11 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public class Student {
-	// ToDo 1: Make this class a child of Person
 	// ToDo 1: Make this class a child of Human
 
 	// ToDo 2: Fix the resulting errors
 
-	// ToDo 3: Add a field for GPA and create setter and getter
+	// ToDo 3: Add a field for GPA and create a setter and a getter
 	
 	// ToDo 4: Add comments to your code
 }
